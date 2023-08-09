ffa4d413b6451b5858ab7cd9da89eaf98e88b0c9
sanish chirayath <sanishchirayath@sanishs-Mac-mini.local>
2023-08-09 21:38:03 +0530

    feat: basic chaining

--- app.js
+++ app.js
@@ -1,1 +1,54 @@
-console.log('this is app.js')
+const { reverse } = require("dns");
+
+require("dotenv").config();
+
+console.log("you are doing great");
+console.log("User Id:", process.env.USER_ID);
+console.log("User Key:", process.env.USER_KEY);
+
+// I am learning how to implemet method chaining
+function magicString(string) {
+  this.string = string;
+}
+
+magicString.prototype = {
+  length: function () {
+    return this.string.length;
+  },
+
+  reverse: function () {
+    this.string =  this.string.split("").reverse().join("");
+    return this;
+  },
+
+  upperCase: function () {
+    this.string =  this.string.toUpperCase();
+    return this;
+  },
+
+  lowerCase: function () {
+    this.string =  this.string.toLowerCase();
+    return this;
+  },
+
+  camelCase: function () {
+    this.string =  this.string
+      .toLowerCase()
+      .split(" ")
+      .map((word) => {
+        return word[0].toUpperCase() + word.slice(1);
+      })
+      .join(" ");
+    return this;
+  },
+
+};
+
+const magic = new magicString("I am learning how to implement method chaining");
+
+console.log('Length: ',magic.length());
+console.log('Reversed string', magic.reverse());
+console.log("Uppercased string", magic.upperCase());
+console.log("Lowercased string", magic.lowerCase());
+console.log("Camelcased string", magic.camelCase());
+console.log("chained methods output", magic.camelCase().lowerCase().reverse().upperCase());
